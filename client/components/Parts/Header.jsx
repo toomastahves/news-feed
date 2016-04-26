d4f304ebe4b8928e129dd8918a9926ef5164c898
Toomas Tahves <toomastahves@hotmail.com>
2016-04-26 16:39:58 +0300

    fixed header

--- client/components/Parts/Header.jsx
+++ client/components/Parts/Header.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export const Header = () => {
   return (
     <div className='header'>
-      {'header'}
+      <div className='hamburger'></div>
+      <a href='https://www.postimees.ee/' target='_blank'><div className='logo'></div></a>
+      <a href='https://github.com/toomastahves/news-feed' target='_blank'><div className='share'></div></a>
     </div>
   );
 };
