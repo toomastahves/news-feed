741b3f249f4cebc41d0da4b806abf6c787182cc0
Toomas Tahves <toomastahves@hotmail.com>
2016-04-28 14:07:17 +0300

    refactored infinite scroll

--- client/events/scroll.js
+++ client/events/scroll.js
@@ -1,35 +1,10 @@
 import store from '../store/';
 import { getArticlesRequest } from '../actions/api';
 
-// http://www.howtocreate.co.uk/tutorials/javascript/browserwindow
-const getScrollXY = () => {
-  let scrOfX = 0;
-  let scrOfY = 0;
-  if(document.body && (document.body.scrollLeft || document.body.scrollTop)) {
-    scrOfX = document.body.scrollLeft;
-    scrOfY = document.body.scrollTop;
-  }
-  return [scrOfX, scrOfY];
-};
-
-// http://james.padolsey.com/javascript/get-document-height-cross-browser/
-const getDocHeight = () => {
-  const D = document;
-  return Math.max(
-      D.body.scrollHeight, D.documentElement.scrollHeight,
-      D.body.offsetHeight, D.documentElement.offsetHeight,
-      D.body.clientHeight, D.documentElement.clientHeight
-  );
-};
-
 let executed = false;
 
-document.addEventListener('scroll', () => {
-
-  // Checking if user has reached bottom of the page, so we can execute another request.
-  if(getDocHeight() <= (getScrollXY()[1] + window.innerHeight)) {
-
-    // Preventing multiple AJAX request in short period of time.
+window.onscroll = function() {
+  if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
     if(!executed) {
       console.log('bottom');
       const offset = store.getState().apiReducer.articleOffset;
@@ -42,4 +17,4 @@ document.addEventListener('scroll', () => {
       }, 500);
     }
   }
-});
+};
